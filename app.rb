84eb632e71309338b3b75a6ca60dde008c397c6e
Rusydy <rusydymuhiddin@gmail.com>
2024-05-29 13:25:41 +0700

    ADD: database connection and ping functionality

--- app.rb
+++ app.rb
@@ -1,5 +1,33 @@
 require 'webrick'
 require 'json'
+require 'pg'
+
+# Database configuration
+DB_CONFIG = {
+  host: 'localhost',
+  dbname: 'postgres',
+  user: 'postgres',
+  password: 'root',
+  port: 5432
+}
+
+# Connect to the database
+begin 
+  conn = PG.connect(DB_CONFIG)
+  puts 'Connected to the database'
+rescue PG::Error => e
+  puts "Unable to connect to the database: #{e.message}"
+  exit
+end
+
+# Ping the database
+begin
+  res = conn.exec('SELECT 1')
+  puts 'Ping the database: ' + res[0]['?column?']
+rescue PG::Error => e
+  puts "Unable to ping the database: #{e.message}"
+  exit
+end
 
 server = WEBrick::HTTPServer.new(:Port => 8000)
 
