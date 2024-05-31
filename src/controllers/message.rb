85351320f64ae7c33b07dd054c5129359ecf1727
Rusydy <rusydymuhiddin@gmail.com>
2024-05-31 12:39:13 +0700

    Add: http_status gem for handling HTTP status codes

--- src/controllers/message.rb
+++ src/controllers/message.rb
@@ -1,9 +1,12 @@
 require 'json'
 require_relative '../services/message'
+require 'http_status'
 
 class MessageController
   def self.greet(req, res, db_connection)
     message = MessageService.get_greeting(db_connection)
+
+    res.status = HTTPStatus::Ok
     res.content_type = 'application/json'
     res.body = { message: message }.to_json
   end
--- src/routes.rb
+++ src/routes.rb
@@ -1,8 +1,15 @@
 require_relative 'controllers/message'
+require 'http_status'
 
 def load_routes(server, db_connection)
-  # Define a basic GET route
+  # Greet
   server.mount_proc '/api/greet' do |req, res|
     MessageController.greet(req, res, db_connection)
+  end
+
+  # Health-check
+  server.mount_proc '/api/health-check' do |req, res|
+    res.status = HTTPStatus::Ok
+    res.body = { status: 'ok'}.to_json
   end
 end
