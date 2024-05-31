b7c3aedca495c079b25d43f16efc452483affcfb
Rusydy <rusydymuhiddin@gmail.com>
2024-05-31 22:22:19 +0700

    Update: seperate validation of `req` with `add_message`

--- src/controllers/message.rb
+++ src/controllers/message.rb
@@ -12,24 +12,13 @@ class MessageController
   end
 
   def self.add_message(req, res)
-    if req.body.nil?
-      res.status = HTTPStatus::BadRequest
-      res.body = { error: 'Body is required' }.to_json
-      return
-    end
+    return unless validate_add_message_request(req, res)
 
     body = JSON.parse(req.body)
-
     message = body['message']
 
-    if message.nil?
-      res.status = HTTPStatus::BadRequest
-      res.body = { error: 'Message is required' }.to_json
-    end
-
     result = MessageService.add_message(message)
-    puts "this is the result #{result}"
-    if result[:error] != nil
+    if result[:error]
       res.status = HTTPStatus::InternalServerError
       res.body = { error: result[:error] }.to_json
       return
@@ -40,5 +29,25 @@ class MessageController
       message: 'Message added',
       id: result[:id]
     }.to_json
+  end
+
+  private
+
+  def self.validate_add_message_request(req, res)
+    if req.body.nil?
+      res.status = HTTPStatus::BadRequest
+      res.body = { error: 'Body is required' }.to_json
+      return false
+    end
+
+    body = JSON.parse(req.body)
+    message = body['message']
+    if message.nil?
+      res.status = HTTPStatus::BadRequest
+      res.body = { error: 'Message is required' }.to_json
+      return false
+    end
+
+    true
   end
 end
