d6e82816a9e447d5f2db77d993c30d02c80d7e88
Rusydy <rusydymuhiddin@gmail.com>
2024-05-31 18:10:54 +0700

    ADD: `add_message` in controller

--- src/controllers/message.rb
+++ src/controllers/message.rb
@@ -10,4 +10,29 @@ class MessageController
     res.content_type = 'application/json'
     res.body = { message: message }.to_json
   end
+
+  def self.add_message(req, res)
+    if req.body.nil?
+      res.status = HTTPStatus::BadRequest
+      res.body = { error: 'Body is required' }.to_json
+      return
+    end
+
+    body = JSON.parse(req.body)
+    puts "The body is #{body}"
+
+    message = body['message']
+
+    if message.nil?
+      res.status = HTTPStatus::BadRequest
+      res.body = { error: 'Message is required' }.to_json
+    end
+
+    # TODO: Implement this method later
+    # MessageService.add_message(message)
+    puts "The message is #{message}"
+
+    res.status = HTTPStatus::Created
+    res.body = { message: 'Message added' }.to_json
+  end
 end
